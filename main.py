21435e27e7636e32f4de759fb808d473460c6c77
Aiden Regan <reganaiden@gmail.com>
2021-04-18 15:21:38 +0100

    Update main.py
    Update 15.21

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ import pandas as pd
 # Notes for next steps
 print ("Begin of project, 18 April 2022")
 # Try push to github
-print ('set draft to github attempt two')
+print ("set draft to github attempt two")
+# Project now set as Main.py on UCDPAaidenregan
+
 
