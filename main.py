edc810beb788796e6d7738c30b9cc055110b1195
Aiden Regan <reganaiden@gmail.com>
2021-04-18 15:11:46 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Prepwork for Fin Project
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+# Notes for next steps
+print ("Begin of project, 18 April 2022")
+# Try push to github
+print ('set draft to github attempt two')
+
