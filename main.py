ba336c0048ad6403a6d67d28e61bfe45583a02a3
Aiden Regan <reganaiden@gmail.com>
2021-04-22 17:24:05 +0100

    22 April #2
    more functions added

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # Finance Project
 import numpy as np
 import matplotlib as plt
-import pandas as pd
-
 # Notes for next steps
 print("Begin of project, 18 April 2022")
-print("sent draft to github attempt two")
+print("The code is sent to github")
 # Project now set as Main.py on UCDPA Aiden Regan
-Sales2018 = pd.read.csv(r"C:\Users\Sarah\Downloads\passengercars2018.csv")
+import pandas as pd
+Sales2018 = pd.read_csv(r"C:\Users\Sarah\Downloads\passengercars2018.csv")
 # Plug in needed for csv read
 print(Sales2018.head())
+print(Sales2018.shape)
 missing_values_count = Sales2018.isnull().sum
-print(missing_values_count[0:5])
+print(missing_values_count)
