9185dc4b804d77f33e14c9b3b202708a74e1ad7e
Aiden Regan <reganaiden@gmail.com>
2021-04-22 16:28:28 +0100

    Update 22 04
    Import csv

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-# Prepwork for Fin Project
+# Finance Project
 import numpy as np
 import matplotlib as plt
 import pandas as pd
+
 # Notes for next steps
-print ("Begin of project, 18 April 2022")
-# Try push to github
-print ("set draft to github attempt two")
-# Project now set as Main.py on UCDPAaidenregan
-
-
+print("Begin of project, 18 April 2022")
+print("sent draft to github attempt two")
+# Project now set as Main.py on UCDPA Aiden Regan
+Sales2018 = pd.read.csv(r"C:\Users\Sarah\Downloads\passengercars2018.csv")
+# Plug in needed for csv read
+print(Sales2018.head())
+missing_values_count = Sales2018.isnull().sum
+print(missing_values_count[0:5])
